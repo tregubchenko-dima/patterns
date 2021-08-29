1e8ba05a133291c2cfebadad7380aa230d0b3126
Tregubchenko Dima <tregubchenko2@yandex.ru>
2021-08-29 22:45:25 +0300

    Added one more example of builder pattern

--- src/com.patterns/builder/BuilderTest.java
+++ src/com.patterns/builder/BuilderTest.java
@@ -1,5 +1,10 @@
 package com.patterns.builder;
 
+import com.patterns.builder.v1.Person;
+import com.patterns.builder.v2.Car;
+import com.patterns.builder.v2.CarDirector;
+import com.patterns.builder.v2.SportCarBuilder;
+
 public class BuilderTest {
 
     public static void main(String[] args){
@@ -11,5 +16,12 @@ public class BuilderTest {
                 .build();
 
         System.out.println(person.toString());
+
+        CarDirector carDirector = new CarDirector();
+        carDirector.setCarBuilder(new SportCarBuilder());
+
+        Car car = carDirector.build();
+
+        System.out.println(car.toString());
     }
 }
