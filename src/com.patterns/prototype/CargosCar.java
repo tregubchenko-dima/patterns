8beb4b2def92fbd90ce8010880f5788411249717
Tregubchenko Dima <tregubchenko2@yandex.ru>
2021-08-31 23:02:21 +0300

    Added prototype pattern

---
+++ src/com.patterns/prototype/CargosCar.java
@@ -0,0 +1,37 @@
+package com.patterns.prototype;
+
+import java.util.Objects;
+
+public class CargosCar extends Transport {
+
+    private int cargoWeight;
+
+    public CargosCar(){
+
+    }
+
+    public CargosCar(CargosCar car) {
+        super(car);
+        this.cargoWeight = car.cargoWeight;
+    }
+
+    @Override
+    public Transport clone() {
+        return new CargosCar(this);
+    }
+
+    public void setCargoWeight(int cargoWeight) {
+        this.cargoWeight = cargoWeight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CargosCar cargosCar = (CargosCar) o;
+        return cargoWeight == cargosCar.cargoWeight;
+    }
+
+
+}
---
+++ src/com.patterns/prototype/PrototypeTest.java
@@ -0,0 +1,26 @@
+package com.patterns.prototype;
+
+public class PrototypeTest {
+
+    public static void main(String[] args){
+        CargosCar cargoCar = new CargosCar();
+        cargoCar.setCargoWeight(10000);
+        cargoCar.setColor("GREEN");
+        cargoCar.setMaxSpeed(100);
+        System.out.println(cargoCar);
+
+        CargosCar cargoCarCopy = (CargosCar) cargoCar.clone();
+        System.out.println(cargoCarCopy);
+        System.out.println(cargoCar.equals(cargoCarCopy));
+
+        SportsCar sportCar = new SportsCar();
+        sportCar.setColor("RED");
+        sportCar.setMaxSpeed(300);
+        sportCar.setTurbochargedEngine(true);
+        System.out.println(sportCar);
+
+        SportsCar sportCarCopy = (SportsCar) sportCar.clone();
+        System.out.println(sportCarCopy);
+        System.out.println(sportCar.equals(sportCarCopy));
+    }
+}
