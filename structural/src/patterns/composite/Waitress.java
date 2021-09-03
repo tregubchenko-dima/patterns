360142126943494e572284092c2c1ed35fbcf1a0
Tregubchenko Dima <tregubchenko2@yandex.ru>
2021-09-03 23:34:27 +0300

    added pattern composite

---
+++ structural/src/patterns/composite/Waitress.java
@@ -0,0 +1,13 @@
+package patterns.composite;
+
+public class Waitress {
+    private MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu(){
+        allMenus.print();
+    }
+}
